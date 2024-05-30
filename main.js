6976e5f9e92f91fefaefd525b01ffaadd1f588ca
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-05-30 21:21:57 +0600

    video added

--- main.js
+++ main.js
@@ -15,3 +15,25 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf);
+
+document.getElementById("video-check").addEventListener("change", (e) => {
+  var iframe = document.getElementById("video");
+  // var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+  // var video = innerDoc.querySelector("video");
+  // var audio = innerDoc.querySelector("audio");
+  // console.log(iframe.src);
+  if (!e.target.checked) {
+    setTimeout(() => (iframe.src = ""), 500);
+  } else {
+    iframe.src =
+      "https://www.youtube.com/embed/xcJtL7QggTI?si=1Foi26HSTSFzNqJT";
+  }
+  // if (video) {
+  //   video.pause();
+  // }
+
+  // if (audio) {
+  //   audio.pause();
+  // }
+});
