f9ce23d068997a1625bf54ccc421703e054760ef
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-03 16:35:55 +0600

    all done with responsiveness

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ import "aos";
 
 AOS.init();
 
-const lenis = new Lenis();
-
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+});
 lenis.on("scroll", (e) => {
   console.log(e);
 });
