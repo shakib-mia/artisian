24c055af5601b119d7a2da44035027256ed1f736
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-04 15:57:19 +0600

    screen issues are fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,8 +9,8 @@ export default {
         sm: "600px",
         md: "728px",
         lg: "984px",
-        xl: "1240px",
-        "2xl": "1640px",
+        xl: "1300px",
+        // "2xl": "1640px",
       },
     },
     extend: {
