e14ae0c96a47ad0a2f40a72b2652c0b1fe342382
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-06-05 09:50:09 +0600

    container fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,13 @@ export default {
     container: {
       center: true,
       padding: "1rem",
-      // screens: {
-      //   sm: "600px",
-      //   md: "728px",
-      //   lg: "984px",
-      //   xl: "1280px",
-      //   // "2xl": "1640px",
-      // },
+      screens: {
+        sm: "600px",
+        md: "728px",
+        lg: "984px",
+        xl: "1300px",
+        // "2xl": "1640px",
+      },
     },
     extend: {
       colors: {
